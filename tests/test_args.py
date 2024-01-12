91daf631452d906e18eba23ed64d20397b655c83
Quinten <quinten.roets@gmail.com>
2024-01-11 22:30:52 -0800

    preserve first sys arg

--- tests/test_args.py
+++ tests/test_args.py
@@ -10,37 +10,45 @@ text_arguments = given(arguments=text_list_strategy)
 
 @no_cli_args
 def test_no_cli_args_decorator() -> None:
-    assert sys.argv == []
+    verify_no_cli_arguments()
 
 
 def test_no_cli_args_context_manager() -> None:
     with no_cli_args:
-        assert sys.argv == []
+        verify_no_cli_arguments()
 
 
 @cli_args()
 def test_cli_args_decorator() -> None:
-    assert sys.argv == []
+    verify_no_cli_arguments()
 
 
 @text_arguments
 def test_cli_args_context_manager(arguments: list[str]) -> None:
     with cli_args(*arguments):
-        assert sys.argv == arguments
+        assert extract_cli_arguments() == arguments
 
 
 @text_arguments
 def test_combined_decorators(arguments: list[str]) -> None:
     with cli_args(*arguments):
-        assert sys.argv == arguments
+        assert extract_cli_arguments() == arguments
     with no_cli_args:
-        assert sys.argv == []
+        verify_no_cli_arguments()
 
 
 @no_cli_args
 @text_arguments
 def test_decorator_context_manager_combination(arguments: list[str]) -> None:
-    assert sys.argv == []
+    verify_no_cli_arguments()
     with cli_args(*arguments):
-        assert sys.argv == arguments
-    assert sys.argv == []
+        assert extract_cli_arguments() == arguments
+    verify_no_cli_arguments()
+
+
+def verify_no_cli_arguments() -> None:
+    assert extract_cli_arguments() == []
+
+
+def extract_cli_arguments() -> list[str]:
+    return sys.argv[1:]
